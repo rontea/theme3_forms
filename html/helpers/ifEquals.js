0fd6ec33222f2875a6d24eba1107d4d373ce23ac
rontea <dupelawebsite@gmail.com>
2024-09-27 17:31:00 -0400

    update(scss): form design and input design for text, theme setting,and reset

--- html/helpers/ifEquals.js
+++ html/helpers/ifEquals.js
@@ -1,31 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  * Helper in getting attributes when use the ifEquals
- * @param {*} id 
- * @param {*} inputs 
+ * @param {*} id
+ * @param {*} inputs
  * @returns attributes
  */
 
-module.exports = function(id, inputs) {
+module.exports = function (id, inputs) {
+  let results = "";
 
-    let results = "";
-   
-    for (let key in inputs) {
-        
-        if (key === 'inputs' && Array.isArray(inputs[key])){
-            
-            inputs[key].forEach((input, index) => {        
-                // Display each key-value pair for the input item
-                if(input.id === id){
-                    for (let inputKey in input) {
-                        results += ` ${inputKey}="${input[inputKey]}" `;
-                    }
-                    
-                }
-
-            });
+  for (let key in inputs) {
+    if (key === "inputs" && Array.isArray(inputs[key])) {
+      inputs[key].forEach((input, index) => {
+        // Display each key-value pair for the input item
+        if (input.id === id) {
+          for (let inputKey in input) {
+            results += ` ${inputKey}="${input[inputKey]}" `;
+          }
         }
+      });
     }
-    return results;
+  }
+  return results;
 };
