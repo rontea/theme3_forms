d8b9764962f51d938362695171842f05649f9ed1
rontea <dupelawebsite@gmail.com>
2024-10-23 15:31:43 -0400

    update(input default and defaut) tested inputs with counter and mutliple counter on same page

--- src/js/app.js
+++ src/js/app.js
@@ -33,38 +33,71 @@ const data = createApp({
 // Mount the app to the #data element
 data.mount('#sample');
 
+
+
+
+document.querySelectorAll('.text-counter').forEach((element, index) => { 
+  const uniqueId = `#text-counter-${index + 1}`;
+  console.log(uniqueId);
+
+  
 const textCounter = createApp({
-  delimiters: ['[[', ']]'],
-  data() {
-    return {
-     userInput: '',
-     charCount: 0
-    };
-  },
-  watch: {
-   userInput() {
-     this.countChecker();
-   }
-  },
-  methods: {
-    countChecker(){
-      const minLength = this.$refs.inputElement.minlength;
-      const maxLength = this.$refs.inputElement.maxlength;
-      const value = this.userInput;
+
+    delimiters: ['[[', ']]'],
+    data() {
+      return {
+      userInput: '',
+      charCount: 0,
+      maxCount: 0
+      };
+    },
+    mounted() {
       const inputElement = this.$refs.inputElement;
-      const helperElement = this.$refs.helperElement;
-
-      this.charCount = value.length;
-      console.log(minLength);
-      console.log(maxLength);
-      console.log(value.length);
-      if (value.length < minLength) {
-        helperElement.classList.add('input__counter--error');
-        console.log('input is too short');
-      }
-
+      const value = inputElement.getAttribute('value');
+      const maxLength = inputElement.getAttribute('maxlength');
+      this.maxCount = maxLength;
+      this.userInput = value;
+    },
+    watch: {
+    userInput() {
+      this.countChecker();
     }
-  }
-});
+    },
+    methods: {
+      countChecker(){
 
-textCounter.mount('#text-counter');
+        const inputElement = this.$refs.inputElement;
+        const helperElement = this.$refs.helperElement;
+        const helperRequired = this.$refs.helperRequired;
+
+        const minLength = inputElement.getAttribute('minlength');
+        const maxLength = inputElement.getAttribute('maxlength');
+        let value = this.userInput;
+      
+        this.charCount = value.length;
+        this.maxCount = maxLength;
+
+        if (value.length < minLength) {
+          helperElement.classList.add('input__counter--error');
+          console.log('input is too short');
+        }else {
+          helperElement.classList.remove('input__counter--error');
+        } 
+
+        if (value.length === minLength) {
+          helperElement.classList.remove('input__counter--error');
+        }
+
+        if(value.length === 0 || value.length < minLength) {
+          helperRequired.classList.add('input__counter--error');
+        }else {
+          helperRequired.classList.remove('input__counter--error');
+        }
+
+      }
+    }
+  });
+
+  textCounter.mount(uniqueId);
+  
+});
